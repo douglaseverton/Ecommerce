3e512ca18b4b0b2b0715ce0df516357f76122233
Everton Douglas Pereira <evertondouglas1993@hotmail.com>
2020-05-05 19:56:49 -0300

    passwordReset model

--- app/Models/PasswordReset.js
+++ app/Models/PasswordReset.js
@@ -11,9 +11,9 @@ class PasswordReset extends Model {
         this.addHook('beforeCreate', async model => {                       //sera criado o token antes de ser registrado no banco de dados
             model.token = await str_random(25)
 
-            const expires_at = new Date() // aqui estamos criando o tempo de expiracão do token
+            const expires_at = new Date() // aqui expire_at esta recebendo uma nova data para  expiracão do token
 
-            expires_at.setMinutes(expires_at.getMinutes() +30)  //vai pegar a hora atual e vai colocar mais 30 minutos para a expiracão do token
+            expires_at.setMinutes(expires_at.getMinutes() +30)  //o expires_at vai pegar a hora atual e vai colocar mais 30 minutos para a expiracão do token
 
             model.expires_at = expires_at  //minha model recebe a const expires_at e diz que esse token tem 30 minutos até expirar 
         })
