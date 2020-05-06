e6421c4b100ea07c9d911814c73f28ca0a8bb699
Everton Douglas Pereira <evertondouglas1993@hotmail.com>
2020-05-06 11:12:31 -0300

    OrderItem model

---
+++ app/Models/OrderItem.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class OrderItem extends Model {
+
+    //como a tabela OrderItens não tem as colunas timestemp que e o mesmo que created_at e updated_at temos que passar a model NoTimestemp que ;e onde tem a funcão que faz a verificacão quando não a timestemp
+
+    static get traits(){
+        return ['App/Models/Traits/NoTimestamp']
+    }
+
+    //relacionamento com produto
+
+    products(){
+        return this.belongsTo('App/Models/Product')
+    }
+
+    orders(){
+        return this.belongsTo('App/Models/Order')
+    }
+}
+
+module.exports = OrderItem
